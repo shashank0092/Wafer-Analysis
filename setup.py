54073e526f8d54865ed535712604c30a5a8a2f3d
SHASHANK <shashank7016204503@gmail.com>
2024-01-21 10:29:07 +0530

    Intial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Basic Learning Of MLOPS Concepts',
+    author='shashank',
+    license='MIT',
+)
